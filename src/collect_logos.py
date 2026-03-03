27811fe5ab5d968f65b04a2c01065e5f832ee7c5
AveryJD <averyjdoiron@icloud.com>
2026-03-03 13:20:31 -0400

    Add collect_logos.py

---
+++ src/collect_logos.py
@@ -0,0 +1,40 @@
+# ====================================================================================================
+# FUNCTION FOR COLLECTING NHL TEAM LOGOS
+# ====================================================================================================
+
+# Imports
+import requests
+import os
+import constants
+
+
+def scrape_logos() -> None:
+    """
+    Iterates through team codes and logo variants to download SVG files.
+    Currently 'ATL' and 'PHX' must be retreived manually
+
+    :return: None
+    """
+    os.makedirs('assets/team_logos', exist_ok=True)
+    
+    for team_code in constants.TEAM_NAMES:
+        for variant in ['light', 'dark']:
+            # Make the full URL for the SVG file
+            file_name = f"{team_code}_{variant}.svg"
+            url = f'https://assets.nhle.com/logos/nhl/svg/{file_name}'
+            
+            # Make a GET request to the URL
+            response = requests.get(url, stream=True)
+            
+            if response.status_code == 200:
+                # Write the SVG content to a file
+                output_path = os.path.join('assets', 'team_logos', file_name)
+                with open(output_path, 'wb') as f:
+                    f.write(response.content)
+                print(f'Saved {file_name}')
+
+def main():
+    scrape_logos()
+
+if __name__ == '__main__':
+    main()
