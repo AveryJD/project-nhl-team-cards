1d8157361322b24001064993aa3e69d51f914aa1
AveryJD <averyjdoiron@icloud.com>
2026-03-16 22:56:17 -0300

    Update scrape_data.py

--- src/scrape_data.py
+++ src/scrape_data.py
@@ -5,11 +5,11 @@
 # Imports
 import pandas as pd
 from bs4 import BeautifulSoup
-import os
 import requests
 import time
 import random
 import constants
+import load_save
 
 
 DATA_DIR = constants.DATA_DIR
@@ -66,13 +66,13 @@ def scrape_data(url: str) -> pd.DataFrame:
     return df
 
 
-def scrape_standings_data(season: str) -> None:
+def scrape_team_data(season: str, situation: str) -> None:
     """
     
     """
 
     url_season = season.replace("-", "")
-    url = f'https://www.naturalstattrick.com/teamtable.php?fromseason={url_season}&thruseason={url_season}&stype=2&sit=all&score=all&rate=n&team=all&loc=B&gpf=410&fd=&td='
+    url = f'https://www.naturalstattrick.com/teamtable.php?fromseason={url_season}&thruseason={url_season}&stype=2&sit={situation}&score=all&rate=n&team=all&loc=B&gpf=410&fd=&td='
 
     df = scrape_data(url)
 
@@ -82,12 +82,8 @@ def scrape_standings_data(season: str) -> None:
     df = df.drop(df.columns[0], axis=1)
 
     # Save as a CSV
-    filename = f'{season}_standings_data.csv'
-    save_dir = os.path.join(DATA_DIR, 'team_card_data', season, 'standings')
-    os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, filename)
-    df.to_csv(save_path, index=False)
-    print(f"Saved {filename}")
+    file_name = f'{season}_{situation}_team_data.csv'
+    load_save.save_csv(df, season, 'results', file_name)
 
     return None
 
@@ -103,50 +99,8 @@ def scrape_games_data(season: str) -> None:
     df = scrape_data(url)
 
     # Save as a CSV
-    filename = f'{season}_game_data.csv'
-    save_dir = os.path.join(DATA_DIR, 'team_card_data', season, 'games')
-    os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, filename)
-    df.to_csv(save_path, index=False)
-    print(f"Saved {filename}")
-
-    return None
-
-
-def scrape_team_data(season: str, situation: str, team_abbreviation: str) -> None:
-    """
-    
-    """
-
-    special_team_map = constants.SPECIAL_TEAM_MAP 
-
-    if team_abbreviation in ['LAK', 'NJD', 'SJS', 'TBL']:
-        url_team = special_team_map.get(team_abbreviation)
-    else:
-        url_team = team_abbreviation
-
-    url_season = season.replace("-", "")
-    url = f'https://www.naturalstattrick.com/playerteams.php?fromseason={url_season}&thruseason={url_season}&stype=2&sit={situation}&score=all&stdoi=std&rate=n&team={url_team}&pos=S&loc=B&toi=0&gpfilt=none&fd=&td=&tgp=410&lines=single&draftteam=ALL#'
-
-    df = scrape_data(url)
-
-    # Skip saving if DataFrame is empty
-    if df.empty:
-        print(f"No data for {team_abbreviation} in {season}")
-        return None
-
-    df['Total Points'] = pd.to_numeric(df['Total Points'])
-    df = df.sort_values(by='Total Points', ascending=False)
-
-    df = df.drop(df.columns[0], axis=1)
-
-    # Save as a CSV
-    filename = f'{season}_{situation}_{team_abbreviation}_team_data.csv'
-    save_dir = os.path.join(DATA_DIR, 'team_card_data', season, f'teams_{situation}', )
-    os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, filename)
-    df.to_csv(save_path, index=False)
-    print(f"Saved {filename} to {save_dir}")
+    file_name = f'{season}_games.csv'
+    load_save.save_csv(df, season, 'results', file_name)
 
     return None
 
@@ -157,15 +111,12 @@ def main():
     
     """
 
-    for season in constants.SEASONS:
+    for season in constants.DATA_SEASONS:
 
-        #scrape_standings_data(season)
-        
-        #scrape_games_data(season)
+        scrape_games_data(season)
 
         for situation in constants.SITUATIONS:
-            for team_abbreviation in constants.TEAM_NAMES:
-                scrape_team_data(season, situation, team_abbreviation)
+            scrape_team_data(season, situation)
 
     return None
 
